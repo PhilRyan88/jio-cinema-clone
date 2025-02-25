836dbdd1f57d345c3aad3adc70f4229be2b43724
PhilRyan88 <88623913+PhilRyan88@users.noreply.github.com>
2025-02-25 19:42:51 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
   return (
     <div>
-      <h1>JioCinema</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
